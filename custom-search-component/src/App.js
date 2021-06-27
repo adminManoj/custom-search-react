5aa6fbad5506c8c05fa0b25d30bf5ccdac5b1647
manoj deshwal <deshwal.03@gmail.com>
2021-06-27 16:02:09 +0530

    pushed all

---
+++ custom-search-component/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CustomSearch from './CustomSearch';
+
+function App() {
+  return (
+    <div className="App">
+     <CustomSearch />
+    </div>
+  );
+}
+
+export default App;
